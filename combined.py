2ac63036279d31f4c77d2f23c9ba190b43f7e03b
Ryderoo7 <125922280+Ryderoo7@users.noreply.github.com>
2023-08-13 22:32:17 +1200

    combined.py

--- combined.py
+++ combined.py
@@ -158,6 +158,10 @@ def signupbutton():
         password = entry_password_signup.get()
         confirm_password = entry_confirm_password_signup.get()
         weight = weight_signup.get()
+        # Makes password length > 6 characters
+        if len(password) < 6:
+            messagebox.showerror("Error", "Password must be at least 6 characters long")
+            return
 
         conn = sqlite3.connect('User+pass.db')
         cursor = conn.cursor()
